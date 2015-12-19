7a89eec12287ae8175d59334b9b284816a899960
Alexander Atwater <Alex@Alexanders-iMac.local>
2015-12-19 15:50:55 -0500

    fixed row alignment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,51 @@
 //button functions
 
+//increment button clicked 
+$(".incrementButton").click(function() {
+
+	//figure out which button it was
+	var val = this.textContent;
+
+	var incVal = 0.0;
+
+	if (val == '+') {
+		incVal = 0.5;
+	} else {
+		incVal = -0.5;
+	};
+
+	//increment the text
+	$('#incrementText').text((parseFloat($('#incrementText').text()) + incVal));
+
+	//increment the dictionary
+	
+	for (var i = gradesArray.length - 1; i >= 0; i--) {
+
+		if (gradesArray[i] != 'E') {
+
+			gradesScale[gradesArray[i]] = (parseFloat(gradesScale[gradesArray[i]]) + incVal);
+
+		};
+
+	};
+
+	//update the view
+	buildGradeScaleTable();
+	var data = {};
+	data['termWeight'] = (100 - $('#finalWeight').val());
+	data['finalWeight'] = $('#finalWeight').val();
+	data['currentAverage'] = $('#currentAverage').val();
+	var results = dataAnalysis(data);
+	
+	//build results screen
+	buildResultsScreen(results);
+
+
+
+
+});
+
+
 $("#goButton").click(function(){
 
 	//data validation
@@ -66,9 +112,7 @@ $("#goButton").click(function(){
 	
 });
 
-$("#showGradeScale").click(function(){
-
-	function createTable() {
+function buildGradeScaleTable() {
 
 		//delete existing contents
 		$("#gradeScaleTable").empty();
@@ -93,6 +137,7 @@ $("#showGradeScale").click(function(){
 		var letterHeader = letterRow.insertCell(0);
 		letterHeader.innerHTML = 'Letter';
 		letterHeader.style.width = '12%';
+		letterHeader.style.minWidth = '67px;';
 		//format
 
 
@@ -110,14 +155,20 @@ $("#showGradeScale").click(function(){
 		var percentHeader = percentRow.insertCell(0);
 		percentHeader.innerHTML = '%';
 
+
+
 	}
+
+$("#showGradeScale").click(function(){
+
+	
 
 	//check if already showing or not
 	if ($('#gradeScaleView').css('display') == 'none') {
 
 		//show
 		
-		createTable();
+		buildGradeScaleTable();
 		$("#gradeScaleView").slideDown();
 
 		//change text
@@ -151,6 +202,7 @@ $("#resultsBackArrow").click(function(){
 function buildResultsScreen(data) {
 
 	///variables
+	
 	// Find a <table> element with id="myTable":
 	var table = document.getElementById("resultsTable");
 
@@ -162,12 +214,39 @@ function buildResultsScreen(data) {
 
 		var cell1 = row.insertCell(0);
 		var cell2 = row.insertCell(1);
-		debugger;
+
+		//to line things up add a <p> element
+
+		
 		// Add some text to the new cells:
-		cell1.innerHTML = firstCol;
+		
+		if (row.rowIndex == 0) {
+			//if header
+			cell1.innerHTML = firstCol;
+		} else {
+			cell1.innerHTML = '<p class="tableAlign">' + firstCol + '</p>';
+		};
+		
 		cell2.innerHTML = secondCol;
+
+		
+
 	}
 	
+	//delete content
+	$("#resultsTable").empty();
+
+	//add header
+	addTableRow('Class Grade', 'Minimum Grade On Final');
+
+
+	for (var i = 0; (data.length - 1) >= i; i++) {
+		var object = data[i];
+		var secondCol = (object['minPossibleGradeOnFinal'] + ' (' + object['minPossibleGradeOnFinalAsLetter'] + '%)');
+		addTableRow(object['classGrade'], secondCol);
+	};
+
+
 
 
 }
